151e2e8b05714383fc7991c8838a1761cbd18f82
Entropy <benbelland@gmail.com>
2020-12-06 03:02:26 -0500

    save point

--- src/hexagon/hexagon.py
+++ src/hexagon/hexagon.py
@@ -13,32 +13,42 @@ import sys
 
 class Window(Gtk.Window):
     def __init__(self):
+        
         self.builder = Gtk.Builder()
         self.builder.add_from_file("../glade/center.glade")
 
 
         self.window = self.builder.get_object("Center")
-
+        
         self.provider = Gtk.CssProvider()
         self.screen = self.window.get_screen()
         self.provider.load_from_path("../glade/style.css")
         self.style = self.window.get_style_context()
         self.style.add_provider_for_screen(self.screen, self.provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
-        self.lines = 40
+        self.lines = 30
         self.c = self.builder.get_object("LineContainer")
-        for line in range(self.lines):
-            l = Gtk.Entry()
-            ls = l.get_style_context()
-            ls.add_class("GtkEntry")
-
-            self.c.add(l)
-
-            self.c.show()
+        self.line = self.builder.get_object("Line")
+        output = ""
+        l = ""
+        file = open("../templates/atLine", "r")
+        l += file.readlines()[0]
+        file.close()
+        
+        for i in range(self.lines):
+            output += l + "\n"
+        self.line.set_label(output)
+        self.line.connect("clicked", self.onClick)
+        #self.c.add(l)
+        self.c.show()
         self.window.connect("destroy", Gtk.main_quit)
         self.title = self.builder.get_object("Titlebar")
 
         self.title.set_label("Nachos")
 
+        self.window.show_all()
+    def onClick(self, button):
+        print("Hallo")
+        self.style.background = "orange"
         self.window.show_all()
     def main(self):
         Gtk.main()
