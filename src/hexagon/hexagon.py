a0a6a65fb1d448a73a57eb3ac940f2d4efe2527e
Entropy <benbelland@gmail.com>
2020-12-07 13:35:05 -0500

    cleanup aisle me

--- src/hexagon/hexagon.py
+++ src/hexagon/hexagon.py
@@ -131,14 +131,11 @@ class Window(Gtk.Window):
                 s = bar.get_style_context()
                 s.add_class("GtkEntry")
                 bar.show()
-                #bar = self.builderEd.get_object("Bar"+str(i))
-                #box = self.builder.get_object("Box"+str(i))
                 if i > 27:
                     box = self.builder.get_object("Box"+str(i-4))
                 else:
                     box = self.builder.get_object("Box"+str(i+1))
                 box.add(bar)
-                #self.show_all()
                 box.show_all()
     def moveNext(self, key, event):
         print("HIT A KEY")
