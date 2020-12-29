7cd3119f60a942bda638f8d32531e406259fdda3
Entropy <7851974-rainbow-punk-treant@users.noreply.gitlab.com>
2020-12-28 20:41:49 -0800

    dooty dooty

--- src/ovoid/ovoid.py
+++ src/ovoid/ovoid.py
@@ -1,12 +1,9 @@
 #!/bin/bash/python3
-
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
-
 import sys
-
 class Pyramid():
     builder = Gtk.Builder()
     def boop(self, button):
@@ -14,15 +11,12 @@ class Pyramid():
             print("If you wish to boop a snoot, please add it as a single string")
         if len(sys.argv) == 2:
             print(sys.argv[1])
-
     def __init__(self):
         self.builder.add_from_file("../glade/speedruneditor.glade")
         self.window = self.builder.get_object("main")
-
         self.window.show_all()
         self.window.connect("destroy", Gtk.main_quit)
         self.button = self.builder.get_object("booper")
-        self.button.connect("clicked", self.boop)
-        
+        self.button.connect("clicked", self.boop)     
         Gtk.main()
 py = Pyramid()
